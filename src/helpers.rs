dfcbef3f7758bbb4a0dbe5324903005d72aab31a
Samarth Hattangady <samhattangady@gmail.com>
2019-09-24 21:26:18 +0530

    wip: working correctly. not aesthetically pleasing

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,10 +1,11 @@
-pub mod helpers {
+pub mod position_helpers {
+    #[derive(Debug, Clone, Copy)]
     pub struct Position {
         pub x: u32,
         pub y: u32,
     }
 
-    pub fn get_closest_point(position: Position, points: &Vec<Position>) -> usize {
+    pub fn get_closest_point(position: &Position, points: &Vec<Position>) -> usize {
         let mut closest_index = 0;
         let mut closest_distance = std::f32::MAX;
         for (i, point) in points.iter().enumerate() {
@@ -24,3 +25,46 @@ pub mod helpers {
     }
 }
 
+pub mod error_helpers {
+    use image::{GenericImage, GenericImageView};
+    use super::position_helpers::Position;
+
+    pub fn get_zone_mse(zone: &Vec<Position>, src: &image::DynamicImage) -> f32 {
+        if zone.len() == 0 {
+            return 0.0;
+        }
+        let zone_average = get_zone_average(&zone, &src);
+        let mut zone_mse = 0.0;
+        for point in zone {
+            let pixel = src.get_pixel(point.x, point.y);
+            let pixel_data = [pixel.0[0], pixel.0[1], pixel.0[2]];
+            zone_mse += get_pixel_mse(&pixel_data, &zone_average)
+        }
+        zone_mse /= zone.len() as f32;
+        zone_mse
+    }
+
+    pub fn get_zone_average(zone: &Vec<Position>, src: &image::DynamicImage) -> [u8; 3] {
+        if zone.len() == 0 {
+            return [0 as u8, 0 as u8, 0 as u8];
+        }
+        let mut zone_sum = [0 as u32, 0 as u32, 0 as u32];
+        for point in zone {
+            let pixel = src.get_pixel(point.x, point.y);
+            zone_sum[0] += pixel.0[0] as u32;
+            zone_sum[1] += pixel.0[1] as u32;
+            zone_sum[2] += pixel.0[2] as u32;
+        }
+        zone_sum = [zone_sum[0]/zone.len() as u32, zone_sum[1]/zone.len() as u32, zone_sum[2]/zone.len() as u32];
+        [zone_sum[0] as u8, zone_sum[1] as u8, zone_sum[2] as u8]
+        
+    }
+
+    fn get_pixel_mse(p1: &[u8; 3], p2: &[u8; 3]) -> f32 {
+         let mut square_error = 0.0;
+         square_error += (p1[0] as f32 - p2[0] as f32).powf(2.0);
+         square_error += (p1[1] as f32 - p2[1] as f32).powf(2.0);
+         square_error += (p1[2] as f32 - p2[2] as f32).powf(2.0);
+         square_error
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,102 @@ use image::{GenericImage, GenericImageView};
 use rand::prelude::*;
 
 mod helpers;
-use helpers::helpers::{Position, get_closest_point};
+use helpers::position_helpers::{Position, get_closest_point};
+use helpers::error_helpers::{get_zone_average, get_zone_mse};
 
+
+struct Voronoi {
+    /// The list of each of the points
+    points: Vec<Position>,
+    /// Each list in zones contains all the pixels present in the
+    /// zone of the point at the same index
+    zones: Vec<Vec<Position>>,
+}
 
 fn main() {
     let src = image::open("images/girl_with_pearl.jpg").unwrap();
-    let img = voronoi(&src);
+    let mut voronoi = initiate_voronoi(&src, 10);
+    for i in 0..2000 {
+        println!("Entering round {:}", i);
+        add_point_to_voronoi(&mut voronoi, &src);
+        recalculate_voronoi_zones(&mut voronoi, &src);
+        if i%50 == 0 {
+            save_voronoi(&voronoi, &src, format!("results/voronoi_{:}.png", i));
+        }
+    }
+    let img = create_voronoi(&src);
     img.save("results/voronoi.png");
 }
 
-fn voronoi(src: &image::DynamicImage) -> image::DynamicImage {
+fn initiate_voronoi(src: &image::DynamicImage, num_points: u32) -> Voronoi {
+    let mut voronoi = Voronoi {
+        points: Vec::new(),
+        zones: Vec::new(),
+    };
+    let (image_width, image_height) = src.dimensions();
+    let mut rng = rand::thread_rng();
+    for _ in 0..num_points {
+        let x = rng.gen_range(0, image_width as u32);
+        let y = rng.gen_range(0, image_height as u32);
+        voronoi.points.push( Position {x: x, y: y} );
+        voronoi.zones.push(Vec::new());
+    }
+    recalculate_voronoi_zones(&mut voronoi, &src);
+    voronoi
+}
+
+fn add_point_to_voronoi(voronoi: &mut Voronoi, src: &image::DynamicImage) {
+    /// This function finds the zone with the highest mse when compared
+    /// to the source image, and replaces that zones point with two points
+    /// in the same zone
+    let mut highest_mse = 0.0;
+    let mut highest_point_index = 0 as usize;
+    for (i, zone) in voronoi.zones.iter().enumerate() {
+        let zone_mse = get_zone_mse(&zone, &src);
+        if zone_mse > highest_mse {
+            highest_mse = zone_mse;
+            highest_point_index = i;
+        }
+    }
+    let mut rng = rand::thread_rng();
+    let high_zone = &voronoi.zones[highest_point_index];
+    let rand_point_index_1 = rng.gen_range(0, high_zone.len() as u32);
+    let rand_point_index_2 = rng.gen_range(0, high_zone.len() as u32);
+    let rand_point_1 = high_zone[rand_point_index_1 as usize].clone();
+    let rand_point_2 = high_zone[rand_point_index_2 as usize].clone();
+    // voronoi.points[highest_point_index] = rand_point_1;
+    voronoi.points.push(rand_point_2);
+    voronoi.zones.push(Vec::new());
+    println!("Highest mse in zone : {:}", highest_point_index);
+}
+
+fn recalculate_voronoi_zones(voronoi: &mut Voronoi, src: &image::DynamicImage) {
+    let (image_width, image_height) = src.dimensions();
+    for i in 0..voronoi.points.len() {
+        voronoi.zones[i].clear();
+    }
+    for x in 0..image_width {
+        for y in 0..image_height {
+            let pos = Position { x:x, y:y };
+            let closest_index = get_closest_point(&pos, &voronoi.points);
+            voronoi.zones[closest_index].push(pos);
+        }
+    }
+}
+
+fn save_voronoi(voronoi: &Voronoi, src: &image::DynamicImage, filename: String) {
+    let (image_width, image_height) = src.dimensions();
+    let mut dest = image::DynamicImage::new_rgb8(image_width, image_height);
+    for zone in &voronoi.zones {
+        let colour = get_zone_average(&zone, &src);
+        for point in zone {
+            dest.put_pixel(point.x, point.y, image::Rgba{ 0:[colour[0], colour[1], colour[2], 255 as u8]});
+        }
+    }
+    dest.save(filename);
+}
+
+fn create_voronoi(src: &image::DynamicImage) -> image::DynamicImage {
     let (image_width, image_height) = src.dimensions();
     let mut dest = image::DynamicImage::new_rgb8(image_width, image_height);
     let mut rng = rand::thread_rng();
@@ -28,7 +114,7 @@ fn voronoi(src: &image::DynamicImage) -> image::DynamicImage {
     }
     for x in 0..image_width {
         for y in 0..image_height {
-            let closest_index = get_closest_point(Position {x:x, y:y}, &points);
+            let closest_index = get_closest_point(&Position {x:x, y:y}, &points);
             let colour = colours[closest_index];
             dest.put_pixel(x, y, image::Rgba{ 0: [colour[0] as u8, colour[1] as u8, colour[2] as u8, 255 as u8]});
         }
